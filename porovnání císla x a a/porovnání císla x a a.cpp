a35e681ed4a853be0785065a23325731e1f0144a
BuildTools <unconfigured@null.spigotmc.org>
2021-10-23 19:33:48 +0200

    Add project files.

---
+++ porovnání císla x a a/porovnání císla x a a.cpp
@@ -0,0 +1,23 @@
+﻿// Program zjistí zda je číslo x větší, rovno a nebo menší než číslo a (5)
+
+#include <stdio.h>
+
+int main() {
+	int x = 4;
+	int a = 5;
+	printf("Cislo %d ", x);
+	if (x - a == 0) {
+		printf("je rovno cislu %d", a);
+		// Je-li číslo x - číslo a 0, program vyhodnotí že se čísla rovnají
+	}
+	else {
+		if (x > a) {
+			printf("je vetsi nez cislo %d", a);
+			// Pokud neplatí předešlá "rovnice", program přejde na další a pokud bude platit tak program vyhodnotí číslo x jako číslo které je větší
+		}
+		else {
+			printf("je mensi nez cislo %d", a);
+			// Jestli že neplatí první a ani druhaá "rovnice", program číslo x vyhodnotí jako číslo menší
+		}
+	}
+}
